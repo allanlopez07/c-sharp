457507df2f5d8c7809ce9dfa773c683d4da1987a
Allan Lopez <allanlopezxd28@gmail.com>
2021-03-01 18:18:38 -0600

    alumno

---
+++ Ejercicios/propiedades/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Juan";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
